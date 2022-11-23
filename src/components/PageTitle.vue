8886ac3a80d4ff8cc352a61f5393a0e40c71f473
Stefano Incerti <ste.ince91@gmail.com>
2022-11-23 13:35:56 +0100

    Initial commit

---
+++ src/components/PageTitle.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "Titolazzo",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style scoped></style>
